333b71977273b0b311b85398f3ab9484c3cb8391
jasongibsonUFL <jasongibson@ufl.edu>
2020-06-29 08:42:15 -0400

    savve

--- simulation/analysis/vasp/calculation.py
+++ simulation/analysis/vasp/calculation.py
@@ -1880,6 +1880,7 @@ class Calculation(models.Model):
         fixed_calc.set_chgcar(calc)
         fixed_calc.write()
         return fixed_calc
+
     '''Machine Learning'''
     def get_soap(self):
         ml=vasp.read_vasp('../mp-90/CONTCAR')
