8492d00ee9d6bcd6fdc91205682c2cc4ddd5c09c
jasongibsonUFL <jasongibson@ufl.edu>
2020-07-01 10:10:24 -0400

    test

--- pages/views.py
+++ pages/views.py
@@ -114,7 +114,10 @@ def database_view(request, *args,**kwargs):
                                         crystal_system=select_crystal_systems, )
         print(type(all_results))
         for a in all_results:
-            print(round(float(a.band_gap),4))
+            if a.band_gap == None:
+                a.band_gap = 0
+            else:
+                a.band_gap = round(float(a.band_gap),4)
 
         context = {
             'all_results': all_results
