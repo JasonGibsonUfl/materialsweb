8e4910dfb4a73ca2d66508bad1a7ac9d24d0062c
jasongibsonUFL <jasongibson@ufl.edu>
2020-07-03 08:07:03 -0400

    sav

--- simulation/analysis/vasp/calculation.py
+++ simulation/analysis/vasp/calculation.py
@@ -191,7 +191,7 @@ class Calculation(models.Model):
         #create DOS
         try:
             dos = DOS(entry=entry)
-            dos.read_doscar(path+'/DOSCAR')
+            #dos.read_doscar(path+'/DOSCAR')
             dos.read(path+'/DOSCAR')
             self.dos = dos
             dos.save()
