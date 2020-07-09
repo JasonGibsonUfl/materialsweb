431d3ac1eaf100ef0dccf50ded220bc83418f1ce
jasongibsonUFL <jasongibson@ufl.edu>
2020-07-09 06:57:10 -0400

    add

--- electronic_visualization/simpleexample.py
+++ electronic_visualization/simpleexample.py
@@ -260,7 +260,7 @@ app.layout = html.Div([
 
 def save_file(name, content):
     """Decode and store a file uploaded with Plotly Dash."""
-    data = content#.encode("utf8").split(b";base64,")[0]
+    data = content.encode("utf8")#.split(b";base64,")[0]
     with open(os.path.join(UPLOAD_DIRECTORY, name), "wb") as fp:
         print(UPLOAD_DIRECTORY)
         fp.write(data)
