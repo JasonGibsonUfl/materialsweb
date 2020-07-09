cc9ef7238b460de6faa80ec4d7f9fff1dca49c98
jasongibsonUFL <jasongibson@ufl.edu>
2020-07-09 08:40:55 -0400

    add

--- electronic_visualization/simpleexample.py
+++ electronic_visualization/simpleexample.py
@@ -313,6 +313,7 @@ def update_output(uploaded_filenames, uploaded_file_contents):
 def get_dos(vasprun_dos):
     ## get CompleteDos object and "save" in hidden div in json format
     print('IN VASP GET DOS')
+    vasprun_dos=ET.fromstring(vasprun_dos)
     print(vasprun_dos)
     dos = Vasprun(vasprun_dos).complete_dos
     return json.dumps(dos.as_dict())
