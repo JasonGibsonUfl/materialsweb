49b753474c8ed8c5a545a9e174c02923993c304a
jasongibsonUFL <jasongibson@ufl.edu>
2020-07-13 12:07:50 -0400

    test

--- simulation/tests.py
+++ simulation/tests.py
@@ -8,6 +8,7 @@ class TestSimulation(TestCase):
     def setUp(self):
         path = '/var/www/materialsweb/static/database/mp-691133'
         self.calculation1 = Calculation().read(path)
+        self.calculation1.create_all(path)
 
     def test_bandgap(self):
         self.assertEquals(self.calculation1.band_gap, 1.0072)
