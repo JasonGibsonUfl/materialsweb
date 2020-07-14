ac411f22b13c05673a2339ecdd727b0d73cce1fc
jasongibsonUFL <jasongibson@ufl.edu>
2020-07-14 09:05:29 -0400

    test

--- pages/tests.py
+++ pages/tests.py
@@ -1,15 +1,13 @@
 from django.test import TestCase
 from selenium import webdriver
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-
 import time
 # Create your tests here.
 
 class TestMaterialswebHome(StaticLiveServerTestCase):
 
     def setUp(self):
-        self.browser = webdriver.Firefox('/var/www/materialsweb/pages/drivers', executable_path= '/var/www/materialsweb/pages/drivers/firefox.sh')
+        self.browser = webdriver.Chrome('./chromedriver')
 
     def tearDown(self):
         self.browser.close()
