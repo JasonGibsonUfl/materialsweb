83d54b6863a470108e1f28ddfdb80b4ce9fc613b
jasongibsonUFL <jasongibson@ufl.edu>
2020-07-14 09:27:08 -0400

    test

--- pages/tests.py
+++ pages/tests.py
@@ -11,11 +11,13 @@ class TestMaterialswebHome(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome('/home/cobra_commander/.wdm/drivers/chromedriver/linux64/83.0.4103.39/chromedriver')
 
+        #self.browser = webdriver.Chrome(ChromeDriverManager().install())
+
         #self.browser = webdriver.Chrome('./chromedriver')
 
     def tearDown(self):
         self.browser.close()
 
     def test_no_projects_alerts_is_displated(self):
-        self.browser.get(self.live_server_url)
+        self.browser.get('https://www.google.com/')#self.live_server_url)
         time.sleep(50)
