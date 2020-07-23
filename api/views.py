ee2e1f91a1807f50b4c5ab74fcf8515843d1bcc6
jasongibsonUFL <jasongibson@ufl.edu>
2020-07-23 11:55:56 -0400

    security

--- api/views.py
+++ api/views.py
@@ -1,14 +1,20 @@
 from rest_framework import viewsets
 from django.http import HttpResponseNotFound, JsonResponse, HttpResponse
-from .serializers import ElementSerializer
+from .serializers import ElementSerializer, CalculationSerializer
 from simulation.materials.element import Element
+from simulation.analysis.vasp.calculation import Calculation
+
 import datetime
 
 class ElementViewSet(viewsets.ModelViewSet):
     queryset = Element.objects.all().order_by('name')
     serializer_class = ElementSerializer
-    
-    
+
+
+class CalculationViewSet(viewsets.ModelViewSet):
+    queryset = Calculation.objects.all().order_by('name')
+    serializer_class = CalculationSerializer
+
 def rest(request):
     """
     Render JSON data for the REST API.
