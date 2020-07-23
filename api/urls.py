b096d31f0618abb3fa7902c078cd31c2180e76e0
jasongibsonUFL <jasongibson@ufl.edu>
2020-07-23 12:07:20 -0400

    security

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,7 @@ from . import views
 router = routers.DefaultRouter()
 router.register(r'elements', views.ElementViewSet)
 router.register(r'calculation', views.CalculationViewSet)
+router.register(r'entry', views.EntryViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
