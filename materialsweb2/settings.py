534d67a0fbe93f93ec1cd7aa253246c9d59287e2
jasongibsonUFL <jasongibson@ufl.edu>
2020-07-23 11:43:58 -0400

    security

--- materialsweb2/settings.py
+++ materialsweb2/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-    #'rest_framework',
+    'rest_framework',
     'simulation',
     'pages',
     'accounts',
