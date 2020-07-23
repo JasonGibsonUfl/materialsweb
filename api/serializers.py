2f51f758fdccd9e3ba3ab3f2f84c87f0bed4dc95
jasongibsonUFL <jasongibson@ufl.edu>
2020-07-23 11:50:47 -0400

    security

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
 from simulation.materials.element import Element
+from simulation.analysis.vasp.calculation import Calculation
 
 
 class ElementSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Element
+        fields = '__all__'
+
+from rest_framework import serializers
+from simulation.materials.element import Element
+
+
+class CalculationSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Calculation
         fields = '__all__'
--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register(r'elements', views.ElementViewSet)
+router.register(r'calculation', views.CalculationViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
