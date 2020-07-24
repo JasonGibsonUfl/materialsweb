9a0364e251aa93c9dbecff09ad2014bc82b69923
jasongibsonUFL <jasongibson@ufl.edu>
2020-07-24 09:54:45 -0400

    rm meta

--- api/filters.py
+++ api/filters.py
@@ -1,9 +1,6 @@
 from django_filters.rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
 from simulation.analysis.vasp.calculation import Calculation
-class DynamicSearchFilter(filters.SearchFilter):
-    def get_search_fields(self, view, request):
-        return request.GET.getlist('search_fields', [])
 
 class CalculationFilter(DjangoFilterBackend,):
     bandgap = filters.RangeFilter(name='bandgap')
