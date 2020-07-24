7494dfa696886ef25902f27a9e4e2a95b34fca75
jasongibsonUFL <jasongibson@ufl.edu>
2020-07-24 10:01:41 -0400

    rm meta

--- api/filters.py
+++ api/filters.py
@@ -2,7 +2,7 @@ from django_filters.rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
 from simulation.analysis.vasp.calculation import Calculation
 
-class CalculationFilter(DjangoFilterBackend,):
+class CalculationFilter(filters.BaseFilterBackend):
     bandgap = filters.RangeFilter(name='bandgap')
 
     class Meta:
