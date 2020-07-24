e284f24bff6b7326ab499784444a81b138036188
jasongibsonUFL <jasongibson@ufl.edu>
2020-07-24 11:18:53 -0400

    rm meta

--- api/filters.py
+++ api/filters.py
@@ -5,11 +5,14 @@ from simulation.analysis.vasp.calculation import Calculation
 
 class CalculationFilter(django_filters.FilterSet):
     band_gap = filters.RangeFilter()
-    lattice_system = django_filters.CharFilter(field_name='output__spacegroup__lattice_system', lookup_expr='contains')
-
+    formation_energy = filters.RangeFilter()
+    spacegroup_number = django_filters.NumberFilter(field_name='output__spacegroup__number', lookup_expr='exact')
+    lattice_system = django_filters.CharFilter(field_name='output__spacegroup__lattice_system', lookup_expr='icontains')
+    
     class Meta:
         model = Calculation
         fields = [
             'band_gap',
+            'formation_energy',
             'lattice_system',
         ]
