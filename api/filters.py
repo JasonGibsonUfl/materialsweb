c602eb2722737429d951f559a990b00fda73ada0
jasongibsonUFL <jasongibson@ufl.edu>
2020-07-24 09:53:20 -0400

    rm meta

--- api/filters.py
+++ api/filters.py
@@ -1,4 +1,4 @@
-from rest_framework import filters
+from django_filters.rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
 from simulation.analysis.vasp.calculation import Calculation
 class DynamicSearchFilter(filters.SearchFilter):
