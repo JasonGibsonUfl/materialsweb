b2300fe0124ea503d2a2607f25e2703331136943
jasongibsonUFL <jasongibson@ufl.edu>
2020-07-24 07:56:20 -0400

    rm meta

--- api/views.py
+++ api/views.py
@@ -39,11 +39,6 @@ class CalculationViewSet(viewsets.ModelViewSet):
     http_method_names = ['get']
     filter_backends = (filters.SearchFilter,)
     search_fields = ['dimension']
-    REST_FRAMEWORK = {
-        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-        'PAGE_SIZE': 100
-    }
-
 
 class DosViewSet(viewsets.ModelViewSet):
     queryset = DOS.objects.all().order_by('id')
