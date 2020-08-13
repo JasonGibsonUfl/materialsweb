9f1524a4691f36413317b1074ff58b670b05e258
jasongibsonUFL <jasongibson@ufl.edu>
2020-08-13 14:41:56 -0400

    l

--- lattice_matching/views.py
+++ lattice_matching/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from lattice_matching.eg_Ima import  StructureMatcher
+from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
+from pymatgen import Structure
+
 from django.core.files.storage import FileSystemStorage
 # Create your views here.
 def lattice_matching_view(request, *args,**kwargs):
@@ -16,6 +19,25 @@ def lattice_matching_view(request, *args,**kwargs):
         user_strain = request.POST.get('user_strain', None)
         a= StructureMatcher(user_input_1, user_input_2, float(user_area), float(user_strain))
         context.update({"data": a})
+        context.update({"structure_1": get_jmol3(user_input_1)})
+
 
 
     return render(request, 'lattice.html', context)
+
+def get_jmol3(structure):
+    structure = Structure.from_str(structure, fmt='poscar')
+    analyzer = SpacegroupAnalyzer(structure)
+    structure = analyzer.get_refined_structure()
+    structure.make_supercell([2, 2, 2])
+    xyz_structure = [str(structure.num_sites),structure.composition.reduced_formula]
+    for site in structure.sites:
+        element = site._species.reduced_formula.replace('2', '')
+        atom = '{} {} {} {}'.format(element, str(site.x), str(site.y),str(site.z))
+        xyz_structure.append(atom)
+    string = str(xyz_structure)
+    string = string.replace('[', '')
+    string = string.replace(']', '')
+    string = string.replace(', ', r'\n')
+    string = string.replace("'", "")
+    return string
