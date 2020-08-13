dbe1f127b82df328bd0e87dd8a51444bd3c5dea9
jasongibsonUFL <jasongibson@ufl.edu>
2020-08-13 13:27:25 -0400

    l

--- lattice_matching/views.py
+++ lattice_matching/views.py
@@ -8,13 +8,12 @@ def lattice_matching_view(request, *args,**kwargs):
     context.update({"is_signed_in": is_signed_in})
 
     if request.method == 'POST':
-        if request.form['submit']:
-            user_input_1 = request.FILES.get('user_input_1', None)
-            user_input_2 = request.FILES.get('user_input_2', None)
-            user_input_1 = user_input_1.read().decode("utf-8")
-            user_input_2 = user_input_2.read().decode("utf-8")
-            a= StructureMatcher(user_input_1,user_input_2,20, 5)
-            print(a)
+        user_input_1 = request.FILES.get('user_input_1', None)
+        user_input_2 = request.FILES.get('user_input_2', None)
+        user_input_1 = user_input_1.read().decode("utf-8")
+        user_input_2 = user_input_2.read().decode("utf-8")
+        a= StructureMatcher(user_input_1,user_input_2,20, 5)
+        print(a)
 
 
     return render(request, 'lattice.html', context)
