dd1064555cd6373b5bb881247dd7beb8680348d7
jasongibsonUFL <jasongibson@ufl.edu>
2020-08-13 10:15:15 -0400

    l

--- lattice_matching/views.py
+++ lattice_matching/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-def lattice_view(request, *args,**kwargs):
+def lattice_matching_view(request, *args,**kwargs):
     context = {}
     is_signed_in = request.user.is_authenticated and not request.user.is_anonymous
     context.update({"is_signed_in": is_signed_in})
