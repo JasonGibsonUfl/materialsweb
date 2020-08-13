a731395eff4e6d9431572b6d3c7f37316c598337
jasongibsonUFL <jasongibson@ufl.edu>
2020-08-13 10:38:22 -0400

    l

--- lattice_matching/views.py
+++ lattice_matching/views.py
@@ -5,4 +5,8 @@ def lattice_matching_view(request, *args,**kwargs):
     context = {}
     is_signed_in = request.user.is_authenticated and not request.user.is_anonymous
     context.update({"is_signed_in": is_signed_in})
+
+    if request.method == 'POST':
+        user_input_1 = request.FILES['user_input_1']
+        print(user_input_1)
     return render(request, 'lattice.html', context)
