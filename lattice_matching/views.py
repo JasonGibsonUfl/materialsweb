afa1573e2c48a3036884b86889a442846f6542da
jasongibsonUFL <jasongibson@ufl.edu>
2020-08-13 13:13:05 -0400

    l

--- lattice_matching/views.py
+++ lattice_matching/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from lattice_matching.eg_Ima import  StructureMatcher
 from django.core.files.storage import FileSystemStorage
 # Create your views here.
 def lattice_matching_view(request, *args,**kwargs):
@@ -9,9 +10,10 @@ def lattice_matching_view(request, *args,**kwargs):
     if request.method == 'POST':
         user_input_1 = request.FILES.get('user_input_1', None)
         user_input_2 = request.FILES.get('user_input_2', None)
-        print(type(user_input_2))
         user_input_1 = user_input_1.read().decode("utf-8")
-        #user_input_2 = user_input_2.read().decode("utf-8")
+        user_input_2 = user_input_2.read().decode("utf-8")
+        a= StructureMatcher(user_input_1,user_input_2,20, 5)
+        print(a)
 
 
     return render(request, 'lattice.html', context)
