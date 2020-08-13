22640b676ef1139febfcee03074688248b518fe4
jasongibsonUFL <jasongibson@ufl.edu>
2020-08-13 10:56:05 -0400

    l

--- lattice_matching/views.py
+++ lattice_matching/views.py
@@ -8,7 +8,7 @@ def lattice_matching_view(request, *args,**kwargs):
 
     if request.method == 'POST':
         user_input_1 = request.FILES['user_input_1']
-        print(type(user_input_1))
+        print(str(user_input_1.read()))
         fs = FileSystemStorage()
         fs.save(user_input_1.name, user_input_1)
 
