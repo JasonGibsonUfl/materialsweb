8659d77c4d9a11015c7699a7f8b07a6d56bf1245
jasongibsonUFL <jasongibson@ufl.edu>
2020-08-13 15:10:55 -0400

    l

--- lattice_matching/views.py
+++ lattice_matching/views.py
@@ -19,7 +19,7 @@ def lattice_matching_view(request, *args,**kwargs):
         user_strain = request.POST.get('user_strain', None)
         a= StructureMatcher(user_input_1, user_input_2, float(user_area), float(user_strain))
         context.update({"data": a})
-        context.update({"structure_1": get_jmol3(user_input_1)})
+        context.update({"structure_1": get_jmol2(user_input_1)})
 
     return render(request, 'test.html', context)
 
@@ -33,6 +33,51 @@ def get_jmol3(structure):
         element = site._species.reduced_formula.replace('2', '')
         atom = '{} {} {} {}'.format(element, str(site.x), str(site.y),str(site.z))
         xyz_structure.append(atom)
+    string = str(xyz_structure)
+    string = string.replace('[', '')
+    string = string.replace(']', '')
+    string = string.replace(', ', r'\n')
+    string = string.replace("'", "")
+    return string
+
+def get_jmol2(structure):
+    from pymatgen.core.operations import SymmOp
+    needs_shift = False
+    structure = Structure.from_str(structure, fmt='poscar')
+    if structure.lattice.a == max(structure.lattice.abc):
+        translation = SymmOp.from_rotation_and_translation(translation_vec=(structure.lattice.a / 2, 0, 0))
+        for site in structure.sites:
+            if site._frac_coords[0] > 0.9 or site._frac_coords[0] < 0.1:
+                needs_shift = True
+        if needs_shift:
+            structure.apply_operation(translation)
+        structure.make_supercell([1, 6, 6])
+    elif structure.lattice.b == max(structure.lattice.abc):
+        translation = SymmOp.from_rotation_and_translation(translation_vec=(0, structure.lattice.b / 2, 0))
+        for site in structure.sites:
+            if site._coords[1] > 0.9 or site._coords[1] < 0.1:
+                needs_shift = True
+        if needs_shift:
+            structure.apply_operation(translation)
+        structure.make_supercell([6, 1, 6])
+    else:
+        translation = SymmOp.from_rotation_and_translation(translation_vec=(0, 0, structure.lattice.c / 2))
+        for site in structure.sites:
+            if site._frac_coords[2] > 0.9 or site._frac_coords[2] < 0.1:
+                needs_shift = True
+        if needs_shift:
+            structure.apply_operation(translation)
+
+        structure.make_supercell([6, 6, 1])
+        print('frac_coord: '+ str(site._frac_coords[2]))
+
+    print(structure.lattice.b)
+    xyz_structure = [str(structure.num_sites),structure.composition.reduced_formula]
+    for site in structure.sites:
+        element = site._species.reduced_formula.replace('2', '')
+        atom = '{} {} {} {}'.format(element, str(site.x), str(site.y),str(site.z))
+        xyz_structure.append(atom)
+    #return '+'.join(xyz_structure)
     string = str(xyz_structure)
     string = string.replace('[', '')
     string = string.replace(']', '')
