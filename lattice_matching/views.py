0a76b7a15bf6f28f96a67452505790fec25f6ea3
jasongibsonUFL <jasongibson@ufl.edu>
2020-08-18 10:15:30 -0400

    save

--- lattice_matching/views.py
+++ lattice_matching/views.py
@@ -18,7 +18,6 @@ def lattice_matching_view(request, *args,**kwargs):
         user_area = request.POST.get('user_area', None)
         user_strain = request.POST.get('user_strain', None)
         a= StructureMatcher(user_input_1, user_input_2, float(user_area), float(user_strain))
-        print((a[1][-1]))
         s3 =a[1][-1].to(fmt='poscar')
         context.update({"data": a})
         context.update({"structure_1": get_jmol2(Structure.from_str(user_input_1, fmt='poscar'))})
