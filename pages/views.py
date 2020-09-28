dbf0fcb50efc3bdae118fc30bd1ccd835f6d645e
jasongibsonUFL <jasongibson@ufl.edu>
2020-09-28 16:23:10 -0400

    update

--- pages/views.py
+++ pages/views.py
@@ -209,6 +209,7 @@ from django.shortcuts import render
 from lattice_matching.eg_Ima import  StructureMatcher
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 from pymatgen import Structure
+from quantumML.rest import MWRester
 
 from django.core.files.storage import FileSystemStorage
 # Create your views here.
@@ -221,49 +222,20 @@ def models_view(request, *args,**kwargs):
         if 'submit' in request.POST:
             i = 0
             user_input_1 = request.FILES.get('user_input_1', None)
-            user_input_2 = request.FILES.get('user_input_2', None)
             user_input_1 = user_input_1.read().decode("utf-8")
-            user_input_2 = user_input_2.read().decode("utf-8")
-            request.session['user_input_1'] = user_input_1
-            request.session['user_input_2'] = user_input_2
-            user_area = request.POST.get('user_area', None)
-            user_strain = request.POST.get('user_strain', None)
-            request.session['user_area'] = user_area
-            request.session['user_strain'] = user_strain
-            request.session['i'] = i
-            a= StructureMatcher(user_input_1, user_input_2, float(user_area), float(user_strain))
-            s3 =a[1][i].to(fmt='poscar')
-            strain_u = a[2][i]
-            strain_v = a[3][i]
-            area = a[4][i]
+            import tempfile
+            with tempfile.NamedTemporaryFile() as temp:
+                mwr = MWRester()
+                temp.write(bytes(user_input_1, encoding='utf-8'))
+                soap = mwr.get_soap(temp.name, fmt='poscar')
+                model = mwr.get_KRR('Cd-Te')
+                result = model.predict(soap)
 
-        if 'next' in request.POST:
-            print('NEXT!!!!!!!!!!!')
-            user_input_1 = request.session.get('user_input_1')
-            user_input_2 = request.session.get('user_input_2')
-            user_area = request.session.get('user_area')
-            user_strain = request.session.get('user_strain')
-            i = request.session.get('i')
-            i = i + 1
-            a= StructureMatcher(user_input_1, user_input_2, float(user_area), float(user_strain))
-            if i == len(a[1]):
-                i = 0
-            request.session['i'] = i
-            print(i)
-            s3 =a[1][i].to(fmt='poscar')
-            strain_u = a[2][i]
-            strain_v = a[3][i]
-            area = a[4][i]
+            request.session['user_input_1'] = user_input_1
+
 
         context.update(({
-            "data": a,
-
-            "strain_u": strain_u,
-            "strain_v": strain_v,
-            "Area": area,
-            "page_c": i+1,
-            "page_t": len(a[1]),
-            "download": s3,
+            "data": result,
         }))
 
 
