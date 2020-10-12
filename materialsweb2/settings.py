72c2312699d5e9f5d9651d014a36f617604221c1
jasongibsonUFL <jasongibson@ufl.edu>
2020-10-12 11:13:45 -0400

    update

--- materialsweb2/settings.py
+++ materialsweb2/settings.py
@@ -197,7 +197,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = ( os.path.join('static'), )
 '''
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
     '/home/jason/'
